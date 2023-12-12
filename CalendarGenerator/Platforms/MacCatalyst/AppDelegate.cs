373bc2440488b7a8544024ca26ca7947c544f08b
Tyler Durr <tylerdurr+github@gmail.com>
2023-12-11 23:11:39 -0600

    Add project files.

---
+++ CalendarGenerator/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CalendarGenerator
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
